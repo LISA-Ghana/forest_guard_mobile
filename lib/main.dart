ce6f66ae29d95529782a7b0a7923aa3d65e5fdea
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-10-29 06:26:42 +0000

    Changed application id

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Forest Guard',
+      title: 'Forest Guard Client',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
